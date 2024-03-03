39afbe094b10a3e6eee63ded1894e6db87df56dd
LucaCorra02 <luca.corradini4@gmail.com>
2024-03-03 21:51:16 +0100

    add challenge text

--- WebSecurity/Web15.py
+++ WebSecurity/Web15.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+"""_summary_
+ In questa challenge la flag è in una delle risorse esterne della pagina accessibile all'indirizzo http://web-15.challs.olicyber.it/. 
+ L'obiettivo è utilizzare find_all e gli altri strumenti messi a disposizione da BeautifulSoup per costruire un elenco delle risorse esterne della pagina, si consiglia quindi di utilizzare la funzione get per scaricarle e di eseguirvi all'interno una ricerca testuale per la stringa flag{   
+"""
+
 resHtml = requests.get("http://web-15.challs.olicyber.it/")
 soap = BeautifulSoup(resHtml.text,'html.parser')
 flag = ""
