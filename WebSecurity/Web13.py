6ad08aacc94044bbe7b2521709e537d558f044aa
LucaCorra02 <luca.corradini4@gmail.com>
2024-03-03 19:39:59 +0100

    add text challenge

--- WebSecurity/Web13.py
+++ WebSecurity/Web13.py
@@ -2,6 +2,12 @@ import requests
 from bs4 import BeautifulSoup 
 import re
 
+"""_summary_
+    In questa challenge le lettere evidenziate nella pagina web accessibile all'URL http://web-13.challs.olicyber.it/ formano la flag. 
+    L'obiettivo è isolarle dal resto della pagina e concatenarle. 
+    Si consiglia di utilizzare il metodo find_all della libreria BeautifulSoup con un filtro adeguato, e di iterare sul risultato.
+"""
+
 resHtml = requests.get("http://web-13.challs.olicyber.it/")
 soap = BeautifulSoup(resHtml.text,'html.parser')
 flag = ""
