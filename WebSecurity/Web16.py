dd6db602a1a3b8a3d45b27b50c3fec3662dee28d
LucaCorra02 <luca.corradini4@gmail.com>
2024-03-04 12:04:19 +0100

    add challenge 16

---
+++ WebSecurity/Web16.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+
+def parseLink(link):
+    return str(link).split('"')[1]
+
+def addPageToVisit(visited,links):
+    for link in links:
+        keyPage = parseLink(link)    
+        visited.append(keyPage)
+
+def isFlag(text):
+    if "{" in text:
+        return True
+    return False
+
+def htmlUrl(url):
+    resHtml = requests.get(url)
+    return BeautifulSoup(resHtml.text,'html.parser')
+  
+def main():
+    baseUrl = "http://web-16.challs.olicyber.it"
+    flag=""
+    toVisit = []
+    visited = {}
+   
+    soap = htmlUrl(baseUrl)
+    links = soap.find_all('a')
+    addPageToVisit(toVisit,links)
+
+    for page in toVisit:
+        if page not in visited:
+            visited[page] = True
+            soap = htmlUrl(baseUrl+page)
+            title = soap.find_all('h1')[0]
+            print("Visiting page:",title)
+            if isFlag(str(title)): 
+                flag = title
+                break
+            addPageToVisit(toVisit,soap.find_all('a'))
+
+    print("Flag: ",flag)
+
+
+main()
+
+
+
+
+
+"""_summary_
+print(visited)
+
+
+   
+"""
+
+    
+
+
+
+
+    
+
